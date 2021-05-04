526abe7954efed8062a913e58be2f1434354f056
Anderegg Lars Robin <larsrobin.anderegg@students.bfh.ch>
2021-05-04 21:44:21 +0200

    edited landing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import LandingPage from '../views/LandingPage.vue'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -8,11 +7,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'LandingPage',
-    component: LandingPage
-  },
-  {
-    path: '/home',
     name: 'Home',
     component: Home
   },
