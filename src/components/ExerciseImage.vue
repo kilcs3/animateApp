1afcd294db302be4e280bcf7df1e56a4e4a830fe
Anderegg Lars Robin <larsrobin.anderegg@students.bfh.ch>
2021-06-02 00:42:22 +0200

    added Components ExerciseImage and ExerciseVideo

---
+++ src/components/ExerciseImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-img
+      :src="getImgUrl(exercise.image)"
+      aspect-ratio="1.5"
+      class="grey lighten-2"
+  >
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "exerciseImage",
+  props: ['exercise'],
+  methods: {
+    getImgUrl: function (pic) {
+      return require('@/assets/images/' + pic);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
