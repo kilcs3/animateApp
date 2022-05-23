640158977ce812835f186b13c41029871e985ba1
minhm <minhman.tran@students.bfh.ch>
2022-05-23 13:58:39 +0200

    added exercise review

--- src/views/ExerciseFinished.vue
+++ src/views/ExerciseFinished.vue
@@ -10,6 +10,30 @@
       >
         <exercise-image :imgSource="exercise.imageFinished" :aspectRatio="1.5"></exercise-image>
         <p class="description">Sie haben die &Uuml;bung erfolgreich beendet!</p>
+        <fieldset class="bewertung">
+          <legend><b>Bewerten Sie Ihr visuelles Vorstellungsvermögen</b></legend>
+
+          <div align="left" style="margin-top:15px">
+            <input type="radio" id="option1" name="Vorstellungsvermögen" value="1">
+            <label for="option1" style="margin-left: 10px">kein Bild</label>
+          </div>
+          <div align="left" style="margin-top:10px">
+            <input type="radio" id="option2" name="Vorstellungsvermögen" value="2">
+            <label for="option2" style="margin-left: 10px">verschwommenes Bild</label>
+          </div>
+          <div align="left" style="margin-top:10px">
+            <input type="radio" id="option3" name="Vorstellungsvermögen" value="3">
+            <label for="option3" style="margin-left: 10px">einigermassen klares Bild</label>
+          </div>
+          <div align="left" style="margin-top:10px">
+            <input type="radio" id="option4" name="Vorstellungsvermögen" value="4">
+            <label for="option4" style="margin-left: 10px">Bild fast so klar wie beim Sehen</label>
+          </div>
+          <div align="left" style="margin-top:10px">
+            <input type="radio" id="option5" name="Vorstellungsvermögen" value="5">
+            <label for="option5" style="margin-left: 10px">Bild so klar wie beim Sehen</label>
+          </div>
+        </fieldset>
       </v-col>
       <v-spacer></v-spacer>
     </v-row>
@@ -67,5 +91,7 @@
 </script>
 
 <style scoped>
-
+  .bewertung{
+    border: none;
+  }
 </style>
